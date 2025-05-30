6d41f04c5f31c0e8a285630d1f779b32984ddfd9
asdasari <as.dasari1988@gmail.com>
2025-05-30 18:29:04 +0530

    updated Employee class

--- src/pack1/Employee.java
+++ src/pack1/Employee.java
@@ -1,5 +1,26 @@
 package pack1;
 
 public class Employee {
+	private Integer employeeId;
+	private String employeeName;
+	private Double employeeSalary;
 	
+	public Integer getEmployeeId() {
+		return employeeId;
+	}
+	public void setEmployeeId(Integer employeeId) {
+		this.employeeId = employeeId;
+	}
+	public String getEmployeeName() {
+		return employeeName;
+	}
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+	public Double getEmployeeSalary() {
+		return employeeSalary;
+	}
+	public void setEmployeeSalary(Double employeeSalary) {
+		this.employeeSalary = employeeSalary;
+	}
 }
